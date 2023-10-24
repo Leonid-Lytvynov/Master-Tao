f231b1655dcb616af33dec183d05acec21f4b615
Leonid-Lytvynov <llskot13373@gmail.com>
2023-10-24 20:29:35 +0300

    your commit text

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,15 @@
-  
+
+$(document).ready(function () {
+   $('.first-screen-slider').slick({
+      dots: true,
+      appendArrows: '.first-screen-nav',
+
+   });
+});
+
+const burger = document.querySelector('.hamburger');
+
+burger.addEventListener('click', function () {
+   burger.classList.toggle('is-active')
+});
+
