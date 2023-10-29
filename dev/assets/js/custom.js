0438274f7fb8d15170dbee146047e9b8177f7a5c
Leonid-Lytvynov <llskot13373@gmail.com>
2023-10-29 10:36:56 +0200

    your commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -20,3 +20,14 @@ burger.addEventListener('click', function () {
    burger.classList.toggle('is-active')
 });
 
+
+document.getElementById('fileInput').onchange = function () {
+   document.getElementById('fileName').innerHTML = this.files[0].name;
+};
+
+const checkButton = document.querySelectorAll('[type="checkbox"]');
+
+checkButton.forEach((item, index) => {
+   index === 0 ? item.closest('.input-box').classList.add('has-checkbox') : null
+
+});
