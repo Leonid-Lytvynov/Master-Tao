7b743d05d4c368a359812cb1fd87b63e47a01009
Leonid-Lytvynov <llskot13373@gmail.com>
2023-11-03 11:47:23 +0200

    advantages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -12,6 +12,34 @@ $(document).ready(function () {
          },
       ]
    });
+
+   mobileOnlySlider("#advantages-slider", true, false, 991);
+
+   function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+      var slider = $($slidername);
+      var settings = {
+         mobileFirst: true,
+         dots: $dots,
+         arrows: $arrows,
+         responsive: [
+            {
+               breakpoint: $breakpoint,
+               settings: "unslick"
+            }
+         ]
+      };
+
+      slider.slick(settings);
+
+      $(window).on("resize", function () {
+         if ($(window).width() > $breakpoint) {
+            return;
+         }
+         if (!slider.hasClass("slick-initialized")) {
+            return slider.slick(settings);
+         }
+      });
+   } // Mobile Only Slider
 });
 
 const burger = document.querySelector('.hamburger');
@@ -30,5 +58,4 @@ const checkButton = document.querySelectorAll('[type="checkbox"]');
 checkButton.forEach((item, index) => {
    item.closest('.calculation-column').classList.add('calculation-column__margin');
    index === 0 ? item.closest('.input-box').classList.add('has-checkbox') : null
-
 });
